97dcbc7028b2c1ba32f33942562eb905d378d552
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-02-10 21:02:54 +0100

    add library

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+/** Keep alphabetically sorted */
+export * from './ButtonAboutProject';
+export * from './ButtonViewModal';
+export * from './DocumentsPageTabs';
+export * from './HeaderProject';
+export * from './InputSearchForm';
+export * from './LayoutBasicProject';
+export * from './SelectCatalog';
+export * from './SelectDocumentoIdentidad';
+export * from './SelectNotaCredito';
+export * from './SelectNotaDebito';
+export * from './SelectTipoComprobanteBasico';
+export * from './SelectTipoIgv';
+export * from './TableEmpty';
+export * from './TableError';
