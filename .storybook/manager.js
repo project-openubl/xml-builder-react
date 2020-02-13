1b7d331f4cdc7d12465c3303fd00de1057562195
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-02-13 13:28:14 +0100

    add theme dark and logo

---
+++ .storybook/manager.js
@@ -0,0 +1,12 @@
+import { addons } from "@storybook/addons";
+import { themes } from "@storybook/theming";
+import logo from "../src/styles/assets/images/logo-navbar.svg";
+
+addons.setConfig({
+  theme: {
+    ...themes.dark,
+    brandTitle: 'XML Builder',
+    brandUrl: 'https://project-openubl.github.io/',
+    brandImage: logo
+  }
+});
--- src/components/ButtonAboutProject/stories/ButtonAboutProject.stories.tsx
+++ src/components/ButtonAboutProject/stories/ButtonAboutProject.stories.tsx
@@ -5,14 +5,14 @@ import { ButtonAboutProject } from "../ButtonAboutProject";
 
 storiesOf("Button | About Project", module).add("Default", () => (
   <React.Fragment>
-    Click me!
-    <ButtonAboutProject
-      version="1.0.0"
-      productName="Mi producto"
-      githubSourceCodeURL="https://github.com/project-openubl/xml-builder"
-      githubIssuesURL="https://github.com/project-openubl/xml-builder/issues"
-      documentationURL="https://project-openubl.gitbook.io/xml-builder/"
-      swaggerApiURL="https://app.swaggerhub.com/organizations/project-openubl"
-    />
+      Click me!
+      <ButtonAboutProject
+        version="1.0.0"
+        productName="Mi producto"
+        githubSourceCodeURL="https://github.com/project-openubl/xml-builder"
+        githubIssuesURL="https://github.com/project-openubl/xml-builder/issues"
+        documentationURL="https://project-openubl.gitbook.io/xml-builder/"
+        swaggerApiURL="https://app.swaggerhub.com/organizations/project-openubl"
+      />
   </React.Fragment>
 ));
